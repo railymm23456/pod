9d1778c57e59dbe73cf899ab373afa736f8205fb
railymm23456 <130125241+railymm23456@users.noreply.github.com>
2023-06-02 01:46:58 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+alert("Welcome to Podcast page of 'ZANA'");
+function Function() {
+    document.getElementById("demo").style.backgroundColor = "green";
+    document.getElementById("demo").innerHTML ="Submitted Successfully!";
+  }
+  
